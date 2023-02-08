b97ba2079fec15d584976d8452117fb94d2b286b
Gio4156 <121972120+Gio4156@users.noreply.github.com>
2023-02-08 10:12:56 -0800

    commit to main

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizantalInput;
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -10)
+        {
+           transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+        horizantalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizantalInput * Time.deltaTime * speed);
+    }
+}
